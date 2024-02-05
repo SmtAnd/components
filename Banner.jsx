b2542160aec6d043ba401eb0c18d798ca13d7868
SmtAnd <142537511+SmtAnd@users.noreply.github.com>
2024-02-06 00:37:01 +0530

    Add files via upload

---
+++ Banner.jsx
@@ -0,0 +1,37 @@
+import React ,{useState} from 'react'
+import{FiMapPin, FiSearch} from "react-icons/fi"
+
+const Banner = ({query, handleInputChange}) => {
+  return (
+    <div className='max-w-screen-2xl container mx-auto xl:px-24 px-4 md:py-20 py-14'>
+        <h1 className='text-5xl font-bold text-primary mb-3'>Find Your<span className='text-blue'> new job</span> today</h1>
+        <p className='text-lg text-black/80 mb-8'>Thousands of jobs in the CSE and technology sectors are waiting for you</p>
+
+        <form>
+            <div className='flex justify-start md:flex-row flex-col md:gap-0 gap-4'>
+                <div className='flex md:rounded-s-md rounded shadow-sm ring-1 ring-inset ring-grey-300
+                focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 md:w-1/2 w-full'>
+                    <input type='text' name='title' id='title' placeholder='What position are you looking for ?'
+                    className='block flex-1 border-0 bg-transparent py-1.5 pl-8 text-grey-900
+                    placeholder:text-grey-400 focus:right-0 sm:text-sm sm:leading-6' 
+                    onChange = {handleInputChange}
+                    value = {query}
+                    />
+                    <FiSearch className=" absolute mt-2.5 ml-2 text-grey-400"/>
+                </div>
+                <div className='flex md:rounded-s-md rounded shadow-sm ring-1 ring-inset ring-grey-300
+                focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 md:w-1/2 w-full'>
+                    <input type='text' name='title' id='title' placeholder='Location'
+                    className='block flex-1 border-0 bg-transparent py-1.5 pl-8 text-grey-900
+                    placeholder:text-grey-400 focus:right-0 sm:text-sm sm:leading-6' 
+                    value = {""}
+                    />
+                    <FiMapPin className=" absolute mt-2.5 ml-2 text-grey-400"/>
+                </div>
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default Banner
---
+++ Navbar.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { FaBarsStaggered, FaXmark } from "react-icons/fa6";
+
+
+
+const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const handleMenuToggler = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+    
+    const navItems = [
+        { path: "/", title: "Start a search" },
+        { path: "/my-job", title: "My Jobs" },
+        { path: "/salary", title: "Salary Estimate" },
+        { path: "/post-job", title: "Post A Job" },
+    ]
+    return (
+        <header className='max-w-screen-2xl container mx-auto xl:px-24 px-24'>
+            <nav className='flex justify-between items-center py-6'>
+                <a href="/" className='flex items-center text-4xl text-blue'>
+                    <img src="/images/GetHired_logo.png"
+                        alt="GetHired"
+                        style={{ width: '70px', height: 'auto' }}
+                    />
+                    <span>GetHired</span>
+                </a>
+                {/* NavItems for large devices */}
+                <ul className='hidden md:flex gap-12'>
+                    {
+                        navItems.map(({ path, title }) => (
+                            <li key={path} className='text-base text-primary'>
+                                <NavLink
+                                    to={path}
+                                    className={({ isActive }) => isActive ? "active" : ""}
+                                >
+                                    {title}
+                                </NavLink>
+
+                            </li>
+                        )
+                        )
+                    }
+                </ul>
+                {/* signup and login btn */}
+                <div className='text-base text-primary font-medium space-x-5 hidden lg:block'>
+                    <Link to="/login" className='py-2 px-5 border rounded'>Log in</Link>
+                    <Link to="/Sign up" className='py-2 px-5 border rounded bg-blue text-white'>Sign up</Link>
+                </div>
+
+                {/* Mobile menu */}
+                <div className='md:hidden block'>
+                    <button onClick={handleMenuToggler}>
+                        {
+                            isMenuOpen ? <FaXmark className='w-5 h-5 text-primary'/>: 
+                            <FaBarsStaggered className='w-5 h-5 text-primary'/>
+                        }
+                    </button>
+                </div>
+                {/* navItems for Mobile */}
+                <div className={`px-4 bg-black py-5 rounded-sm ${isMenuOpen ? "" : "hidden"}`}>
+                    <ul>
+                    {
+                        navItems.map(({ path, title }) => (
+                            <li key={path} className='text-base text-white first:text-white py-1'>
+                                <NavLink
+                                    to={path}
+                                    className={({ isActive }) => isActive ? "active" : ""}
+                                >
+                                    {title}
+                                </NavLink>
+                                </li>
+                            ))}
+                            <li className='text-white py-1'><Link to="/login" >Log in</Link></li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
